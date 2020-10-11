f8c6bc746b93b18dcb8cba48b11322b55e8d7446
musta7188 <musta7188@hotmail.it>
2020-10-11 14:19:44 +0100

    first commit

---
+++ src/welcome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function welcome(){
+
+  return(
+    <div>
+     <h1 style={{color:'red'}}>Welcome back mofucers</h1> 
+    </div>
+  )
+}
+
+
+export default welcome
