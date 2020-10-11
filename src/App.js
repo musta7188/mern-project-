bb43c1070822da7abdfccbef59a5279c0940ef8e
musta7188 <musta7188@hotmail.it>
2020-10-11 23:40:32 +0100

    image component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Welcome from './welcome'
+import Image from  './image'
 function App() {
   return (
     <div className="App">
       <Welcome/>
+      <Image/>
     </div>
   );
 }
---
+++ src/image.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const image = () =>{
+  return <div>
+  <img src="https://cms-assets.tutsplus.com/uploads/users/988/posts/30002/image/presentation-software-A.jpg" alt="Italian Trulli"/>
+  </div>
+}
+
+export default image
